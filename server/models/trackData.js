b67613b4bf878d43b5e1720123f93662615dd855
Vahe Yavrumian <vaheyavrumyan@gmail.com>
2019-05-02 18:10:53 +0400

    Fixed expiration time bug

--- server/models/trackData.js
+++ server/models/trackData.js
@@ -1,18 +1,26 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const {MongoClient, ObjectID} = require('mongodb');
 	Schema = mongoose.Schema;
-const exp = process.env.EXP_TIME * 24 * 3600
+const exp = process.env.EXP_TIME
 const trackDataSchema = new Schema({
 	partId: {type: String, unique: true, required: true},
 	techName: {type: Schema.Types.ObjectId, ref: 'Technician', required: true},
 	trackCode: {type: String, unique: true, required: true},
 	courier: {type: String, required: true},
-	delivered: Date,
+	delivered: {type: Date, expires: exp+'m'},
 	startDate: String,
 	lastDate: String
 });
 
-trackDataSchema.index({ delivered: 1 }, { expireAfterSeconds: exp });
+
+
+
 
 const TrackData = mongoose.model('trackData', trackDataSchema)
 
+TrackData.collection.dropIndex('delivered_1', function(err, result) {
+    if (err) {
+        console.log('Error in dropping index!', err);
+    }
+});
 module.exports = {TrackData}
